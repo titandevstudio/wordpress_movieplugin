10be9dbb5bafbd7169cd0ccadd6d9e1091edde52
Javier Castillo <javiercastillo2708@gmail.com>
2018-09-11 11:26:17 -0400

    Basic plugin settings

---
+++ titan-moviepro-plugin/titan-moviepro-plugin.php
@@ -0,0 +1,13 @@
+<?php
+   /*
+   Plugin Name: Movie Pro
+   Plugin URI: https://github.com/titandevstudio/wordpress_movieplugin
+   description: >-
+  A plugin to create sliders and carousels based on movie information gathered 
+  via API call to The Movie DB's API.
+   Version: 0.1
+   Author: Javier Castillo
+   Author URI: 
+   License: MIT
+   */
+?>
