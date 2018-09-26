102015796f0daec339dbd187418718b87cced0b8
Javier Castillo <javiercastillo2708@gmail.com>
2018-09-26 11:35:54 -0400

    Added licence file

--- titan-moviepro-plugin/titan-moviepro-plugin.php
+++ titan-moviepro-plugin/titan-moviepro-plugin.php
@@ -1,13 +1,16 @@
 <?php
-   /*
-   Plugin Name: Movie Pro
-   Plugin URI: https://github.com/titandevstudio/wordpress_movieplugin
-   description: >-
-  A plugin to create sliders and carousels based on movie information gathered 
-  via API call to The Movie DB's API.
-   Version: 0.1
-   Author: Javier Castillo
-   Author URI: 
-   License: MIT
+   /**
+    * @since             1.0.0
+    * @package           Salandash-user-editor
+    * 
+    * @wordpress-plugin
+    * Plugin Name: Movie Pro
+    * Plugin URI: https://github.com/titandevstudio/wordpress_movieplugin
+    * Description: A plugin to create sliders and carousels based on movie information gathered via API call to The Movie DB's API.
+    * Version: 0.1
+    * Author: Javier Castillo
+    * Author URI: 
+    * License: MIT
+     
    */
 ?>
