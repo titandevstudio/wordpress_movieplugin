2710e88500be29d2f8daeea735a643b8d9f019f7
Javier Castillo <javiercastillo2708@gmail.com>
2019-01-21 16:24:56 -0400

    Movie carousel using Slick

---
+++ titan-moviepro-plugin/js/moviepro.js
@@ -0,0 +1,44 @@
+/*
+========================================================
+Slick slider configuration
+========================================================
+*/
+(function($){
+	$(document).ready(function(){
+		$('.moviepro-list').slick({
+		  dots: false,
+		  infinite: true,
+		  speed: 300,
+		  slidesToShow: 4,
+		  slidesToScroll: 1,
+		  centerMode: true,
+		  centerPadding: '0px',
+		  variableWidth: true,
+		  prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left fa-2x"></i></button>',
+		  nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right fa-2x"></i></button>',
+		  responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		        infinite: true,
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }]
+		});
+	});
+})(jQuery)
--- titan-moviepro-plugin/titan-moviepro-plugin.php
+++ titan-moviepro-plugin/titan-moviepro-plugin.php
@@ -13,13 +13,39 @@
 * License: MIT
 */
 
+//==============================================================
 // If this file is called directly, abort.
+//==============================================================
 if ( ! defined( 'WPINC' ) ) {
      die;
 }
+
 if(!class_exists('WP_List_Table')){
    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
+
+// ==============================================
+//  Get Plugin Version
+// ==============================================
+
+function titan_moviepro_plugin_version() {
+  if ( ! function_exists( 'get_plugins' ) )
+    require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+  $plugin_folder = get_plugins( '/' . plugin_basename( dirname( __FILE__ ) ) );
+  $plugin_file = basename( ( __FILE__ ) );
+  return $plugin_folder[$plugin_file]['Version'];
+}
+
+function titan_moviepro_enqueue_resources() {
+  wp_enqueue_style( 'moviepro_style', plugins_url( '/css/moviepro.css' , __FILE__ ), array(), titan_moviepro_plugin_version(), 'all');
+  wp_enqueue_style( 'moviepro_carousel_style', plugins_url( '/css/slick/slick.css' , __FILE__ ), array(), '1.9', 'all');
+  wp_enqueue_style( 'fontAwesome', plugins_url( '/css/fontAwesome/css/all.min.css' , __FILE__ ), array(), '5.6.3', 'all');
+
+  wp_enqueue_script( 'moviepro_carousel_script', plugin_dir_url( __FILE__ ) . 'js/slick/slick.min.js', array('jquery'), '1.9', true );
+  wp_enqueue_script( 'moviepro_main_script', plugin_dir_url( __FILE__ ) . 'js/moviepro.js', array('jquery'), titan_moviepro_plugin_version(), true );
+}
+add_action('wp_enqueue_scripts', 'titan_moviepro_enqueue_resources');
+
 // Include the dependencies needed to instantiate the plugin.
 foreach ( glob( plugin_dir_path( __FILE__ ) . 'admin/*.php' ) as $file ) {
     include_once $file;
@@ -76,14 +102,14 @@ function create_movie_list($movies){
   $data = json_decode($movies);
   $baseURL = 'https://image.tmdb.org/t/p/';
   $imgSize = 'w300';
-  $ul = '<ul class="moviepro-list">';
+  $list = '<div class="moviepro-list">';
   foreach ($data->results as $movie) {
-    $ul .= '<li class="moviepro-listItem">'
+    $list .= '<div' . ' id="'. $movie->id . '"' .' class="moviepro-listItem">'
             . '<img src="' . $baseURL . $imgSize . $movie->{"poster_path"} . '" ' . 'alt="' . $movie->title . '"' . '/>'
-            . '</li>';
+            . '</div>';
   }
-  $ul .= '</ul>';
-  return $ul;
+  $list .= '</div>';
+  return $list;
 }
 
 // "https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=50c1cc8af5a5e07e52ed728d348a4919"
